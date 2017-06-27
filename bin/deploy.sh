049ebcad69772d48e0bcff86faa98e9d8f9a8c2f
Michael Pace and TJ Taylor <pair+mpace+ttaylor@pivotal.io>
2017-06-27 16:08:58 -0600

    Update deploy script to be directory agnostic

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,4 +2,4 @@
 
 $(dirname $0)/../gradlew assemble -Dorg.gradle.project.version=1.0.$PATCH_NUM
 /tmp/cf login -a "$CF_API" -u "$CF_USER" -p "$CF_PASSWORD" -s "$CF_SPACE"
-/tmp/cf push "$CF_APPNAME" -p build/libs/rivendell-1.0.$PATCH_NUM.jar
+/tmp/cf push "$CF_APPNAME" -p $(dirname $0)/../build/libs/rivendell-1.0.$PATCH_NUM.jar
