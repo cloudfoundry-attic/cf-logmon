b457329785275bd9feb372c99b550cc300e4ec8d
Michael Pace and TJ Taylor <pair+mpace+ttaylor@pivotal.io>
2017-06-30 13:28:46 -0600

    Update deploy script to dynamically find CF binary

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+CF=$(which cf)
+if [[ -z "$CF" ]]; then
+  CF=/tmp/cf
+fi
+
 $(dirname $0)/../gradlew assemble -Dorg.gradle.project.version=1.0.$PATCH_NUM
-/tmp/cf login -a "$CF_API" -u "$CF_USER" -p "$CF_PASSWORD" -s "$CF_SPACE"
-/tmp/cf push "$CF_APPNAME" -p $(dirname $0)/../build/libs/rivendell-1.0.$PATCH_NUM.jar
+$CF login -a "$CF_API" -u "$CF_USER" -p "$CF_PASSWORD" -s "$CF_SPACE"
+$CF push "$CF_APPNAME" -p $(dirname $0)/../build/libs/rivendell-1.0.$PATCH_NUM.jar -b https://github.com/cloudfoundry/java-buildpack
